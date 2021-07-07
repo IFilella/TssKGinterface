0427a87684f730c862040752a809b3ab06608bfc
Isaac Filella-Merce <isaac.filella-merce@pasteur.fr>
2021-07-07 16:43:28 +0200

    All data added (queries, alignments, dataframes etc...)

---
+++ results.py
@@ -0,0 +1,3 @@
+# Author: Isaac Filella, Riccardo Pellarin -- isaac.filella-merce@pasteur.fr
+# https://www.linkedin.com/in/isaacfilella/
+# 2019-07-18 11:42:30 (UTC+0100)
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+# Author: Isaac Filella, Riccardo Pellarin -- isaac.filella-merce@pasteur.fr
+# https://www.linkedin.com/in/isaacfilella/
+# 2019-07-18 11:42:30 (UTC+0100)
+
 import fastaf
 import closeness
 import pylab
